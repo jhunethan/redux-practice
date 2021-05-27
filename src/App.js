ae1ab2848c473954eb153eae0a0b9670e73faa03
jhunethan <jhunethan2812@gmail.com>
2021-05-28 00:04:01 +0100

    working state counter and actions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './css/App.css';
+import "./css/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={()=>dispatch(increment(1))}>+</button>
+      <button onClick={()=>dispatch(decrement(1))}>-</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import allReducers from "./reducers/index";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
